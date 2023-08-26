43d4078b3d34212dfd8f89f5b27e6b34f0452589
Jhorman Orozco <jsojhor@gmail.com>
2023-08-26 12:57:51 -0500

    test cases ok

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'siste-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('siste-app');
+  it('should have the correct title', () => {
+    expect(component.title).toBe('siste-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should render the title in the template', () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('siste-app app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.title').textContent).toContain('siste-app');
   });
 });
--- src/app/pages/home/contact/contact.component.spec.ts
+++ src/app/pages/home/contact/contact.component.spec.ts
@@ -1,21 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { SocialLink } from 'src/app/models/socialMedia.model';
 import { ContactComponent } from './contact.component';
 
 describe('ContactComponent', () => {
-  let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let component: ContactComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ContactComponent]
-    });
+      declarations: [ContactComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display social links', () => {
+    const mockSocialLinks: SocialLink[] = [
+      { title: 'Link 1', icon: 'icon1.png', url: 'https://example.com/link1' },
+      { title: 'Link 2', icon: 'icon2.png', url: 'https://example.com/link2' },
+    ];
+    component.socialLinks = mockSocialLinks;
+
+    fixture.detectChanges();
+
+    const linkElements = fixture.debugElement.queryAll(By.css('.social-link'));
+    expect(linkElements.length).toBe(mockSocialLinks.length);
+
+    linkElements.forEach((linkElement, index) => {
+      const link = linkElement.nativeElement as HTMLAnchorElement;
+      const socialLink = mockSocialLinks[index];
+      expect(link.getAttribute('href')).toBe(socialLink.url);
+      expect(link.querySelector('img')?.getAttribute('src')).toContain(socialLink.icon);
+      expect(link.querySelector('img')?.getAttribute('alt')).toBe(socialLink.title);
+    });
+  });
+
+  it('should not display social links if input is empty', () => {
+    fixture.detectChanges();
+
+    const linkElements = fixture.debugElement.queryAll(By.css('.social-link'));
+    expect(linkElements.length).toBe(0);
   });
 });
